d8ad1ede4b395971edc7d260040c2c57da60dc9a
YugandharSarath <yugandhar.sarath53@gmail.com>
2024-09-23 16:17:40 +0530

    wxploring get, post, del

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 
-const app = express(); 
+const app = express();
 
-app.use((req, res) => { 
-    res.send("Hello from the server");
+//Order of the routes matters a lot
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Yugandhar", lastName: "Sarath" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
 });
 
 app.listen(3000, () => {
-    console.log("Server is successfully listening on port 3000");
+  console.log("Server is successfully listening on port 3000");
 });
