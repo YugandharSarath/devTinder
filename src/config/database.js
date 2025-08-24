06b158f7f501218c41455a39d671b668bdfe7bb3
SarathValluru <sarath@hyrgpt.com>
2025-08-24 20:17:59 +0530

    updated DB password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://YugandharSarath:Yugandhar%4053@cluster0.oubf8.mongodb.net/devTinder"
+    "mongodb+srv://YugandharSarath:gafE4P7tCkaz7nay@cluster0.oubf8.mongodb.net/devTinder"
   );
 };
 
