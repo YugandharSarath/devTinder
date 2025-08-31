c8a2ffc102118fbd832f7a4280eab94ecf73a24a
SarathValluru <sarath@hyrgpt.com>
2025-08-31 17:21:26 +0530

    prod site update

--- src/app.js
+++ src/app.js
@@ -1,30 +1,36 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const User = require("./models/user");
-const { validateSignupData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const { userAuth } = require("./middlewares/auth");
 const cors = require("cors");
 require("dotenv").config();
-
-const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(cookieParser());
-
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestsRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
+
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",       
+  "https://sarathdevtinder.xyz", 
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, 
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
@@ -34,10 +40,13 @@ app.use("/", userRouter);
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
-    app.listen(process.env.PORT, () => {
-      console.log("Server is successfully listening on port 7777");
+
+    const PORT = process.env.PORT || 7777;
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.error("Database connection failed");
+    console.error("Database connection failed", err);
   });
+
